a46e6e911ac9394cb1bdd16234d5b0ed890ddcc8
Jiaqiang Ruan <jiaqiang.ruan96@gmail.com>
2021-04-05 12:43:45 -0400

    RJQ: rm old file

--- HW3/run.sh
+++
@@ -1,9 +0,0 @@
-ROOT=/Users/jiaqiangruan/tmp/SearchEngine
-HW=HW3
-LIB=$ROOT/lucene-8.1.1
-CLASSPATH=$LIB/*:$ROOT/$HW
-PARAM_DIR=$ROOT/$HW/PARAM_DIR
-
-java -cp $CLASSPATH QryEval $PARAM_DIR/HW3-Exp-1a.param
-java -cp $CLASSPATH QryEval $PARAM_DIR/HW3-Exp-1b.param
-java -cp $CLASSPATH QryEval $PARAM_DIR/HW3-Exp-1c.param
